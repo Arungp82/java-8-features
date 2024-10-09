9f2620c78d5e7182bf67bd9c56facd2bd039a508
Arungp82 <arungp82@hotmail.com>
2024-10-09 06:11:57 +0530

    New class added

---
+++ src/main/java/com/java8/CalcAvgSalAndRoundNearInt.java
@@ -0,0 +1,23 @@
+package com.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CalcAvgSalAndRoundNearInt {
+
+    public static void main(String[] args) {
+        List<Employee> employeeList = Arrays.asList(
+                new Employee(1,"George","CSC",65000L),
+                new Employee(1,"Singh","IT",80000L),
+                new Employee(1,"Kumar","ECE",70000L),
+                new Employee(1,"Ram","Mech",50000L),
+                new Employee(1,"Suresh","Civil",45000L),
+                new Employee(1,"Ramesh","VLSI",90000L)
+        );
+
+        // To Calculate average salary and round up to nearest Integer
+
+
+    }
+
+}
